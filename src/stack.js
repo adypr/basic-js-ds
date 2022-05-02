c69d2a464fac12be5285ea8d9ef495296de92d3c
Adypr <workady2015@gmail.com>
2022-05-02 19:12:17 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.stackList = [];
+  }
+  push(element) {
+    if (element) this.stackList.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stackList) {
+      let result = this.stackList.pop();
+      return result;
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stackList) return this.stackList[this.stackList.length - 1];
   }
 }
 
