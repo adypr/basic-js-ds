b6e6434e9cfd548caf26e8c8d55ded022a7c7ec6
Adypr <workady2015@gmail.com>
2022-05-02 11:29:52 +0300

    feat: add remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -14,17 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ * Singly - linked lists are already defined using interface*/
+ class ListNode {
+   constructor(x) {
+     this.value = x;
+     this.next = null;
+   }
+ }
+
+function listLength (list, length = 1) {
+    if (!list) return 0;
+    if (list.next === null) return length;
+   return listLength(list.next, length += 1);
+ }
+
+function removeOnce(list, num) {
+  if (!list) return;
+  if (list.value === num) return list.next;
+  if (list.next && list.next.value === num) {
+  list.next = list.next.next;
+  
+  return list;
+  }
+  list.next = removeOnce(list.next, num);
+  if (list.next === undefined) list.next = null;
+  return list;
+}
+
+function removeKFromList(l, k ) {
+  if (listLength(l) === listLength(removeOnce(l, k))) return l;
+  return removeKFromList(removeOnce(l, k), k );
 }
 
 module.exports = {
